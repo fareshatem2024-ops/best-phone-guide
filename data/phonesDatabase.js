db15158d1da2c01a355a0c0036ed800daf94c214
fareshatem2024-ops <fareshatem2024@gmail.com>
2026-02-10 23:04:12 +0200

    Add comprehensive smartphone database

---
+++ data/phonesDatabase.js
@@ -0,0 +1,124 @@
+let phonesDatabase = [
+    {
+        brand: 'Apple',
+        model: 'iPhone 14',
+        specifications: {
+            display: '6.1 inches',
+            processor: 'A15 Bionic',
+            ram: '6 GB',
+            storage: '128/256/512 GB',
+            battery: '3279 mAh',
+            camera: '12 MP (dual)',
+            os: 'iOS 16'
+        },
+        price: 799,
+        rating: 4.7
+    },
+    {
+        brand: 'Samsung',
+        model: 'Galaxy S23',
+        specifications: {
+            display: '6.1 inches',
+            processor: 'Snapdragon 8 Gen 2',
+            ram: '8/12 GB',
+            storage: '128/256/512 GB',
+            battery: '3900 mAh',
+            camera: '50 MP (triple)',
+            os: 'Android 13'
+        },
+        price: 799,
+        rating: 4.8
+    },
+    {
+        brand: 'Xiaomi',
+        model: 'Xiaomi 13',
+        specifications: {
+            display: '6.36 inches',
+            processor: 'Snapdragon 8 Gen 2',
+            ram: '8/12 GB',
+            storage: '128/256 GB',
+            battery: '4500 mAh',
+            camera: '50 MP (triple)',
+            os: 'Android 13'
+        },
+        price: 749,
+        rating: 4.7
+    },
+    {
+        brand: 'OnePlus',
+        model: 'OnePlus 11',
+        specifications: {
+            display: '6.7 inches',
+            processor: 'Snapdragon 8 Gen 2',
+            ram: '8/16 GB',
+            storage: '128/256 GB',
+            battery: '5000 mAh',
+            camera: '50 MP (triple)',
+            os: 'Android 13'
+        },
+        price: 699,
+        rating: 4.6
+    },
+    {
+        brand: 'Google',
+        model: 'Pixel 7',
+        specifications: {
+            display: '6.3 inches',
+            processor: 'Google Tensor G2',
+            ram: '8 GB',
+            storage: '128/256 GB',
+            battery: '4355 mAh',
+            camera: '50 MP (dual)',
+            os: 'Android 13'
+        },
+        price: 599,
+        rating: 4.5
+    },
+    {
+        brand: 'Oppo',
+        model: 'Oppo Find X5',
+        specifications: {
+            display: '6.55 inches',
+            processor: 'Snapdragon 888',
+            ram: '8/12 GB',
+            storage: '256 GB',
+            battery: '4800 mAh',
+            camera: '50 MP (triple)',
+            os: 'Android 12'
+        },
+        price: 999,
+        rating: 4.4
+    },
+    {
+        brand: 'Realme',
+        model: 'Realme GT 2 Pro',
+        specifications: {
+            display: '6.7 inches',
+            processor: 'Snapdragon 8 Gen 1',
+            ram: '8/12 GB',
+            storage: '128/256 GB',
+            battery: '5000 mAh',
+            camera: '50 MP (triple)',
+            os: 'Android 12'
+        },
+        price: 749,
+        rating: 4.5
+    },
+    {
+        brand: 'Huawei',
+        model: 'Huawei P50',
+        specifications: {
+            display: '6.3 inches',
+            processor: 'Snapdragon 888',
+            ram: '8 GB',
+            storage: '128/256 GB',
+            battery: '4360 mAh',
+            camera: '50 MP (triple)',
+            os: 'HarmonyOS 2'
+        },
+        price: 799,
+        rating: 4.6
+    }
+];
+
+module.exports = phonesDatabase;
