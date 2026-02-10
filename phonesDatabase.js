79771c233d4e4f8aeb72215cec09bb971c8e03c4
fareshatem2024-ops <fareshatem2024@gmail.com>
2026-02-10 23:06:06 +0200

    Updated the phonesDatabase.js with a comprehensive list of smartphones.

---
+++ phonesDatabase.js
@@ -0,0 +1,70 @@
+const phonesDatabase = [
+  {
+    brand: "Apple",
+    model: "iPhone 14 Pro",
+    specifications: {
+      display: "6.1 inches, Super Retina XDR",
+      processor: "A16 Bionic",
+      ram: "6GB",
+      storage: "128GB/256GB/512GB/1TB",
+      camera: "12MP + 12MP + 12MP",
+      battery: "3095 mAh",
+      price: "$999"
+    }
+  },
+  {
+    brand: "Samsung",
+    model: "Galaxy S22 Ultra",
+    specifications: {
+      display: "6.8 inches, Dynamic AMOLED",
+      processor: "Exynos 2200 / Snapdragon 8 Gen 1",
+      ram: "8GB/12GB",
+      storage: "128GB/256GB/512GB/1TB",
+      camera: "108MP + 10MP + 10MP + 12MP",
+      battery: "5000 mAh",
+      price: "$1199"
+    }
+  },
+  {
+    brand: "Xiaomi",
+    model: "12 Pro",
+    specifications: {
+      display: "6.73 inches, AMOLED",
+      processor: "Snapdragon 8 Gen 1",
+      ram: "8GB/12GB",
+      storage: "256GB/512GB",
+      camera: "50MP + 50MP + 50MP",
+      battery: "4600 mAh",
+      price: "$999"
+    }
+  },
+  {
+    brand: "Oppo",
+    model: "Find X5 Pro",
+    specifications: {
+      display: "6.7 inches, AMOLED",
+      processor: "Snapdragon 8 Gen 1",
+      ram: "12GB",
+      storage: "256GB",
+      camera: "50MP + 50MP + 13MP",
+      battery: "5000 mAh",
+      price: "$1099"
+    }
+  },
+  {
+    brand: "Vivo",
+    model: "X80 Pro",
+    specifications: {
+      display: "6.78 inches, AMOLED",
+      processor: "Snapdragon 8 Gen 1",
+      ram: "12GB",
+      storage: "256GB/512GB",
+      camera: "50MP + 50MP + 12MP + 8MP",
+      battery: "4700 mAh",
+      price: "$999"
+    }
+  },
+  // Include similar entries for other brands and models ...
+];
+
+export default phonesDatabase;
